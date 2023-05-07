4e758fe1ed74375033c20535d16574a00194446f
João Artur Magalhães <joaoartur4590@gmail.com>
2023-05-06 23:13:20 -0300

    finalizado

---
+++ Arquivos.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Arquivos {
+    public String ReadData(String caminho, Boolean repeatLines) {
+        String conteudo = "";
+        Map<String, String> nonRepeatedData = new HashMap<>();
+        try {
+            FileReader arq = new FileReader(caminho);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha = "";
+            try {
+                linha = lerArq.readLine();
+                while (linha != null) {
+                    if (!repeatLines) {
+                        String[] aux = linha.split(";");
+                        nonRepeatedData.put(aux[0], aux[1] + "\n");
+                    } else {
+                        conteudo += linha + "\n";
+                    }
+                    linha = lerArq.readLine();
+                }
+                arq.close();
+
+                if (!repeatLines) {
+                    Set keys = nonRepeatedData.keySet();
+                    Iterator iterator = keys.iterator();
+                    while (iterator.hasNext()) {
+                        Object aux = iterator.next();
+                        conteudo += aux + ";" + nonRepeatedData.get(aux);
+                    }
+                }
+                return conteudo;
+            } catch (IOException ex) {
+                System.out.println("Erro: Não foi possível ler o arquivo!");
+                return "";
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Erro: Arquivo não encontrado!");
+            return "";
+        }
+    }
+
+    public boolean WriteData(String Caminho, String texto) {
+        try {
+            FileWriter arq = new FileWriter(Caminho);
+            PrintWriter gravarArq = new PrintWriter(arq);
+            gravarArq.write(texto);
+            gravarArq.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public String CombineData(String Caminho1, String Caminho2) {
+        String dataArch1 = this.ReadData(Caminho1, false);
+        String dataArch2 = this.ReadData(Caminho2, false);
+        String combinetedData = dataArch1 + "" + dataArch2;
+
+        Boolean isWritted = this.WriteData("dadosCombinados.txt", combinetedData);
+        combinetedData = this.ReadData("dadosCombinados.txt", false);
+        Boolean isWritted2 = this.WriteData("dadosCombinados.txt", combinetedData);
+
+        if (isWritted && isWritted2) {
+            System.out.println("Arquivos combinados com sucesso");
+        }
+
+        return combinetedData;
+    }
+}
---
+++ TP01.java
@@ -0,0 +1,26 @@
+public class TP01 {
+
+    public static void main(String[] args) {
+        String arq1 = "dados.txt";
+        String arq2 = "dados2.txt";
+        Arquivos arquivo = new Arquivos();
+
+        // Lendo apenas os dados do primeiro arquivo
+        // String texto = arquivo.ReadData(arq1, false);
+        // if (texto.isEmpty())
+        // System.out.println("Erro ao ler do arquivo!");
+        // else
+        // System.out.println(texto);
+
+        // Lendo apenas os dados do segundo arquivo
+        // String texto2 = arquivo.ReadData(arq2, false);
+        // if (texto.isEmpty())
+        // System.out.println("Erro ao ler do arquivo!");
+        // else
+        // System.out.println(texto2);
+
+        String combinatedData = arquivo.CombineData(arq1, arq2);
+        System.out.println(combinatedData);
+
+    }
+}
